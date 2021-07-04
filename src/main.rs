abf61cbc080d643b890c408403c90be4bdf086c7
Stanislav Solovyev <st.solp@gmail.com>
2021-07-04 17:10:57 +0300

    Refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,15 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let pattern = &args[1];
-    let filename = &args[2];
+    let (pattern, filename) = parse_confing(&args);
 
     let file = fs::read_to_string(filename).expect("File not found");
 
     println!("{} \n{}", filename, file);
+}
+
+fn parse_confing(args: &[String]) -> (&str, &str) {
+    let pattern = &args[1];
+    let filename = &args[2];
+    (pattern, filename)
 }
