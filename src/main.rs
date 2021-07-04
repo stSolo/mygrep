ba0b490127b8fac6ce611132ec1190151d3b85c7
Stanislav Solovyev <st.solp@gmail.com>
2021-07-04 14:19:02 +0300

    Split env data

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,7 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let pattern = &args[1];
+    let filename = &args[2];
+    println!("{} {}", pattern, filename);
 }
